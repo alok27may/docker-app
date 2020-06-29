61966aa5d923fa434618d5764554bb3d7b04f866
Alok.Kumar@cdk.com <Alok.Kumar@cdk.com>
2020-06-29 12:54:18 +0530

    added code for communicating between dockers through resttemplate

--- src/main/java/com/docker/app/controller/HomeController.java
+++ src/main/java/com/docker/app/controller/HomeController.java
@@ -1,17 +1,47 @@
 package com.docker.app.controller;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home(final Model model) {
-        model.addAttribute("title", "Docker with Spring Boot and ThemeLeaf !!");
+        final String title = getTitle();
+        model.addAttribute("title", StringUtils.hasText(title) ? title : "Docker with Spring Boot and ThemeLeaf !!");
         model.addAttribute("msg", "Welcome to docker containerized application");
         return "index";
+    }
+
+    private String getTitle() {
+        String baseUrl = "http://producer:8080/";
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response=null;
+        try{
+            response=restTemplate.exchange(baseUrl,
+                    HttpMethod.GET, getHeaders(),String.class);
+        }catch (Exception ex)
+        {
+            System.out.println(ex);
+        }
+        return response == null ? null : response.getBody();
+    }
+
+    private static HttpEntity<?> getHeaders() throws IOException {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
+        return new HttpEntity<>(headers);
     }
 
 
